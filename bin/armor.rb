da40105ab25a92ada1fa361b9b370aa8eee9f91d
Ken Toler <ken.toler@nvisiumsecurity.com>
2014-02-27 11:15:34 -0500

    adjusted files to close

--- bin/armor.rb
+++ bin/armor.rb
@@ -25,28 +25,35 @@ class ArmoredCar
     #creates encrypted file
     File.new("#{@directory}untitledenc.txt", "w+").write(ciphertext)
     #creates nonce file for user to use in decryption
-    File.new("#{@directory}verificationtag", "w+").write(nonce)  
+    noncefile = File.new("#{@directory}verificationtag", "w+")
+    noncefile.write(nonce)
+    noncefile.close  
   end
   
   def unloader(file)
     #open / decrypt file
     puts "Where is your keyfile?"
-    keyfile = gets.chomp
+    keyfile = File.open(gets.chomp)
+    key = File.read(keyfile)
+    keyfile.close
 
     #asks for verificationtag(nonce) from user
     puts "Where is your verification tag?"
-    noncefile = gets.chomp
+    noncefile = File.open(gets.chomp)
+    nonce = File.read(noncefile)
+    noncefile.close
     
     #instantiates secret box named car/
-    car = RbNaCl::SecretBox.new(File.read(keyfile))
+    car = RbNaCl::SecretBox.new(key)
 
 
     #Read the ciphertext from given file
-    target = File.open(file).read
+    targetfile = File.open(file)
+    target = targetfile.read
+    targetfile.close
 
     #Decrypts File
-    message = car.decrypt(File.read("#{@directory}verificationtag"),target)
-
+    message = car.decrypt(nonce,target)
     File.new("#{@directory}untitleddecoded.txt", "w+").write(ciphertext)
   end
   
