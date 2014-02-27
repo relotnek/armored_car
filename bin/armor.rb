f33b308e2d157a1cd92a0437e1e1e95d8e67c3b1
Ken Toler <ken.toler@nvisiumsecurity.com>
2014-02-27 11:58:46 -0500

    Working!

--- bin/armor.rb
+++ bin/armor.rb
@@ -1,60 +1,66 @@
 require 'rbnacl'
 class ArmoredCar
   def directify
-    puts "What's our directory?"
-    @directory = gets.chomp
+    puts "Specify a working directory where keys and verification tags will be saved:"
+    @directory = gets.chomp!
   end
 
   def loader(file)
     #grabs a keyfile from the user
     puts "Where is your keyfile?"
-    keyfile = gets.chomp
+    keyfile = File.open(gets.chomp!)
+    key = File.read(keyfile)
+    keyfile.close
 
     #instantiates secret box named car
-    car = RbNaCl::SecretBox.new(File.read(keyfile))
+    car = RbNaCl::SecretBox.new(key)
+   
     
     #creates a one time value based on the new secret box
     nonce = RbNaCl::Random.random_bytes(car.nonce_bytes)
     
     #reads message to encrypt
-    target = File.open(file).read
+    targetfile = File.open(file)
+    target = File.read(targetfile)
+    targetfile.close
     
     #encrypts file
     ciphertext = car.encrypt(nonce,target)
     
     #creates encrypted file
-    File.new("#{@directory}untitledenc.txt", "w+").write(ciphertext)
+    cipherfile = File.new("#{@directory}untitledenc.txt", "w+")
+    cipherfile.write(ciphertext)
+    cipherfile.close 
     #creates nonce file for user to use in decryption
     noncefile = File.new("#{@directory}verificationtag", "w+")
     noncefile.write(nonce)
-    noncefile.close  
+    noncefile.close 
   end
   
   def unloader(file)
     #open / decrypt file
     puts "Where is your keyfile?"
-    keyfile = File.open(gets.chomp)
+    keyfile = File.open(gets.chomp!)
     key = File.read(keyfile)
     keyfile.close
-
-    #asks for verificationtag(nonce) from user
-    puts "Where is your verification tag?"
-    noncefile = File.open(gets.chomp)
-    nonce = File.read(noncefile)
-    noncefile.close
     
     #instantiates secret box named car/
     car = RbNaCl::SecretBox.new(key)
 
+    #asks for verificationtag(nonce) from user
+    puts "Where is your verification tag?"
+    noncefile = File.open(gets.chomp!)
+    nonce = File.read(noncefile)
+    noncefile.close
 
     #Read the ciphertext from given file
     targetfile = File.open(file)
-    target = targetfile.read
+    target = File.read(targetfile)
     targetfile.close
 
     #Decrypts File
     message = car.decrypt(nonce,target)
-    File.new("#{@directory}untitleddecoded.txt", "w+").write(ciphertext)
+    File.new("#{@directory}untitleddecoded.txt", "w+").write(message)
   end
   
   def transport
@@ -78,7 +84,7 @@ puts "GENERATING"
 x.generate
 
 puts "LOAD: Give the absolute path of your file:"
-x.loader(gets.chomp)
+x.loader(gets.chomp!)
 
 puts "DECRYPT: Give the absolute path of your encrypted file:"
-x.unloader(gets.chomp)
+x.unloader(gets.chomp!)
