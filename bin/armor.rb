37e6fbcaa7516cd848b4605539adb6cd0b72abf0
relotnek <kentoler@Kenneths-MacBook-Pro.local>
2014-02-26 20:30:22 -0500

    started on unloader(decryption)

--- bin/armor.rb
+++ bin/armor.rb
@@ -1,8 +1,13 @@
 require 'rbnacl'
+
 class ArmoredCar
   def loader(file)
     #grabs a key from the user, for now generates random key
+    puts "We're carving out a key for you:"
+    puts "Here's your key:"
     key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    
+    puts "Keep this key safe, this is how you'll open the armored car."
     
     #instantiates secret box named car
     car = RbNaCl::SecretBox.new(key)
@@ -22,6 +27,17 @@ class ArmoredCar
   
   def unloader(file)
     #open / decrypt file
+    puts "Key please..."
+    key = gets.chomp
+    
+    #instantiates secret box named car
+    car = RbNaCl::SecretBox.new(key)
+    
+    #creates a one time value based on the new secret box
+    nonce = RbNaCl::Random.random_bytes(car.nonce_bytes)
+    
+    ciphertext = File.open(file).read
+    puts car.decrypt(nonce,ciphertext)
     
   end
   
@@ -33,5 +49,8 @@ end
 
 
 x = ArmoredCar.new()
-x.loader("/Users/kentoler/Development/Ruby/armored_car/tests/untitled.txt")
+puts "Specify the absolute location of your file:"
+x.loader(gets.chomp!)
 
+puts "Specify the absolute locaton of the encrypted file:"
+x.unloader(gets.chomp!)
